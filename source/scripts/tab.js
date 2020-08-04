d5ff9b0bef1b66990ba706b0d08667ba0072b542
Egor Dubchenko <dubchenkoegor@gmail.com>
2020-08-05 01:20:19 +0300

    Added colors mode

--- source/scripts/tab.js
+++ source/scripts/tab.js
@@ -1,7 +1,65 @@
 import browser from 'webextension-polyfill'
 import { caclInterval } from './interval.js'
 
-document.addEventListener('DOMContentLoaded', async () => {
+const colorModes = {
+  light: {
+    text: '#000',
+    background: '#fff',
+    primary: 'hsl(240, 100%, 57%)',
+    secondary: 'hsl(260, 100%, 57%)',
+    accent: 'hsl(280, 100%, 57%)',
+    muted: '#f9f9fc',
+    gray: '#555'
+  },
+  black: {
+    text: '#fff',
+    background: '#000',
+    primary: '#0ff',
+    secondary: '#0fc',
+    accent: '#f0f',
+    muted: '#111',
+    gray: '#888'
+  },
+  dark: {
+    text: '#fff',
+    background: 'hsl(180, 5%, 15%)',
+    primary: 'hsl(180, 100%, 57%)',
+    secondary: 'hsl(50, 100%, 57%)',
+    accent: 'hsl(310, 100%, 57%)',
+    muted: 'hsl(180, 5%, 5%)',
+    gray: 'hsl(180, 0%, 70%)'
+  },
+  deep: {
+    text: '#fff',
+    background: 'hsl(230,25%,18%)',
+    primary: 'hsl(260, 100%, 80%)',
+    secondary: 'hsl(290, 100%, 80%)',
+    highlight: 'hsl(260, 20%, 40%)',
+    accent: 'hsl(290, 100%, 80%)',
+    muted: 'hsla(230, 20%, 0%, 20%)',
+    gray: 'hsl(210, 50%, 60%)'
+  },
+  hack: {
+    text: 'hsl(120, 100%, 75%)',
+    background: 'hsl(120, 20%, 10%)',
+    primary: 'hsl(120, 100%, 40%)',
+    secondary: 'hsl(120, 50%, 40%)',
+    accent: 'hsl(120, 100%, 90%)',
+    muted: 'hsl(120, 20%, 7%)',
+    gray: 'hsl(120, 20%, 40%)'
+  },
+  pink: {
+    text: 'hsl(350, 80%, 10%)',
+    background: 'hsl(350, 100%, 90%)',
+    primary: 'hsl(350, 100%, 50%)',
+    secondary: 'hsl(280, 100%, 50%)',
+    accent: 'hsl(280, 100%, 20%)',
+    muted: 'hsl(350, 100%, 88%)',
+    gray: 'hsl(350, 40%, 50%)'
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
   init()
 })
 
@@ -9,6 +67,9 @@ async function init () {
   // browser.storage.sync.clear()
 
   const { dateOfBirth } = await browser.storage.sync.get()
+  const { colorMode } = await browser.storage.local.get()
+
+  setColorMode(colorMode)
 
   if (!dateOfBirth) {
     try {
@@ -33,6 +94,38 @@ async function init () {
   }
 
   startLoop({ dateOfBirth })
+
+  const colorModeButton = document.getElementById('colorMode')
+
+  colorModeButton.addEventListener('click', async () => {
+    const modes = Object.keys(colorModes)
+
+    let { colorMode } = await browser.storage.local.get()
+
+    if (!colorMode) {
+      colorMode = modes[0]
+    }
+
+    const currentColorModeIndex = modes.indexOf(colorMode)
+
+    const nextColorModeIndex = (currentColorModeIndex + 1) % modes.length
+
+    browser.storage.local.set({
+      colorMode: modes[nextColorModeIndex]
+    })
+
+    setColorMode(modes[nextColorModeIndex])
+  })
+}
+
+function setColorMode (mode) {
+  const documentElement = document.documentElement
+
+  const colors = colorModes[mode]
+
+  for (const color in colors) {
+    documentElement.style.setProperty(`--${color}`, colors[color])
+  }
 }
 
 function startLoop ({ dateOfBirth }) {
